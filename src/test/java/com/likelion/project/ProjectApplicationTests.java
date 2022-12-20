d1193614180745a7b6ac29a410ba97fc31ce2bf4
percyfrank <percykwon@gmail.com>
2022-12-20 09:57:06 +0900

    first commit

---
+++ src/test/java/com/likelion/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
