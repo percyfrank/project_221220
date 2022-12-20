0fb01c4cf07cdb7a445f31657ce85a483af9baba
percyfrank <percykwon@gmail.com>
2022-12-20 23:34:49 +0900

    ADD : 포스트 엔티티 생성

---
+++ src/main/java/com/likelion/project/domain/entity/Post.java
@@ -0,0 +1,27 @@
+package com.likelion.project.domain.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class Post extends BaseEntity{
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String title;
+    private String body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
