f73163d0e2a0cad92d8b8549bf4ada4a301321e7
percyfrank <percykwon@gmail.com>
2022-12-23 09:46:08 +0900

    feat : 커스텀 @WithMockUser, WithMockUserSecurityContextFactory클래스 추가

---
+++ src/test/java/com/likelion/project/controller/WithMockCustomUser.java
@@ -0,0 +1,16 @@
+package com.likelion.project.controller;
+
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithMockCustomUserSecurityContextFactory.class)
+public @interface WithMockCustomUser {
+
+    String username() default "username";
+
+    String grade() default "USER";
+
+}
