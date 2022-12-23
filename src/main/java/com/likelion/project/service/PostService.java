c00aed921fe128af50cb6e6f0920711919e46d1b
percyfrank <percykwon@gmail.com>
2022-12-23 09:39:46 +0900

    refactor : exception 클래스 변경, update 메서드 부분 변경 1. AppException으로 변경 2. update 메서드 Post 엔티티 반환

--- src/main/java/com/likelion/project/service/PostService.java
+++ src/main/java/com/likelion/project/service/PostService.java
@@ -4,8 +4,8 @@ import com.likelion.project.domain.dto.post.*;
 import com.likelion.project.domain.entity.Post;
 import com.likelion.project.domain.entity.User;
 import com.likelion.project.exception.ErrorCode;
-import com.likelion.project.exception.PostException;
-import com.likelion.project.exception.UserException;
+import com.likelion.project.exception.AppException;
+import com.likelion.project.exception.AppException;
 import com.likelion.project.repository.PostRepository;
 import com.likelion.project.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -25,11 +25,12 @@ public class PostService {
     private final UserRepository userRepository;
 
 
+    @Transactional
     public PostCreateResponse createPost(PostCreateRequest request, String userName) {
 
         // Authentication으로 넘어온 userName 확인, 없으면 등록 불가
         User user = userRepository.findByUserName(userName)
-                .orElseThrow(() -> new PostException(ErrorCode.INVALID_PERMISSION));
+                .orElseThrow(() -> new AppException(ErrorCode.INVALID_PERMISSION));
 
         Post post = Post.builder()
                 .title(request.getTitle())
@@ -41,53 +42,56 @@ public class PostService {
         return PostCreateResponse.of(savedPost);
     }
 
+    @Transactional
     public Page<PostResponse> findAllPost(Pageable pageable) {
         Page<Post> posts = postRepository.findAll(pageable);
         return posts.map(PostResponse::of);
     }
 
+    @Transactional
     public PostDetailResponse findPost(Integer id) {
         // post 조회
         Post post = postRepository.findById(id)
-                .orElseThrow(() -> new PostException(ErrorCode.POST_NOT_FOUND));
+                .orElseThrow(() -> new AppException(ErrorCode.POST_NOT_FOUND));
 
         return PostDetailResponse.of(post);
-
     }
 
     @Transactional
-    public void update(Integer id, String userName, PostUpdateRequest request) {
+    public Post update(Integer id, String userName, PostUpdateRequest request) {
         // Authentication으로 넘어온 userName 확인, 없으면 수정 불가
         userRepository.findByUserName(userName)
-                .orElseThrow(() -> new UserException(ErrorCode.USERNAME_NOT_FOUND));
+                .orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_FOUND));
 
         // 수정할 포스트 존재 확인
         Post post = postRepository.findById(id)
-                .orElseThrow(() -> new PostException(ErrorCode.POST_NOT_FOUND));
+                .orElseThrow(() -> new AppException(ErrorCode.POST_NOT_FOUND));
 
         // 포스트를 수정한 사용자와 포스트를 최초 작성한자의 동일성 검증
         if (!post.getUser().getUserName().equals(userName)) {
-            throw new PostException(ErrorCode.INVALID_PERMISSION);
+            throw new AppException(ErrorCode.INVALID_PERMISSION);
         }
 
         post.setTitle(request.getTitle());
         post.setBody(request.getBody());
+        Post savedPost = postRepository.save(post);
+        return savedPost;
     }
 
-
+    @Transactional
     public Integer delete(Integer id, String userName, PostDeleteRequest request) {
 
         // Authentication으로 넘어온 userName 확인, 없으면 삭제 불가
         userRepository.findByUserName(userName)
-                .orElseThrow(() -> new UserException(ErrorCode.USERNAME_NOT_FOUND));
+                .orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_FOUND));
 
         // 삭제할 포스트 존재 확인
         Post post = postRepository.findById(id)
-                .orElseThrow(() -> new PostException(ErrorCode.POST_NOT_FOUND));
+                .orElseThrow(() -> new AppException(ErrorCode.POST_NOT_FOUND));
 
         // 포스트를 삭제할 사용자와 포스트를 작성한자의 동일성 검증
         if (!post.getUser().getUserName().equals(userName)) {
-            throw new PostException(ErrorCode.INVALID_PERMISSION);
+            throw new AppException(ErrorCode.INVALID_PERMISSION);
         }
 
         postRepository.deleteById(post.getId());
