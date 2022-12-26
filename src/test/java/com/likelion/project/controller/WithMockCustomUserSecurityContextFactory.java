a4ff0c23547fad94bdf439c004fd4f7d19f28652
percyfrank <percykwon@gmail.com>
2022-12-26 13:19:01 +0900

    refactor : WithMockCustomUserSecurityContextFactory 일부 수정

--- src/test/java/com/likelion/project/controller/WithMockCustomUserSecurityContextFactory.java
+++ src/test/java/com/likelion/project/controller/WithMockCustomUserSecurityContextFactory.java
@@ -1,5 +1,7 @@
 package com.likelion.project.controller;
 
+import com.likelion.project.domain.dto.user.UserJoinRequest;
+import com.likelion.project.service.UserService;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
@@ -9,12 +11,29 @@ import org.springframework.security.test.context.support.WithSecurityContextFact
 import java.util.List;
 
 public class WithMockCustomUserSecurityContextFactory implements WithSecurityContextFactory<WithMockCustomUser> {
+
+    private final UserService userService;
+
+    public WithMockCustomUserSecurityContextFactory(UserService userService) {
+        this.userService = userService;
+    }
+
     @Override
-    public SecurityContext createSecurityContext(WithMockCustomUser annotation) {
-        final SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+    public SecurityContext createSecurityContext(WithMockCustomUser customUser) {
+
+        String username = customUser.username();
+        String password = "password";
+
+        UserJoinRequest userJoinRequest = UserJoinRequest.builder().userName(username).password(password).build();
+        userService.join(userJoinRequest);
+
         final UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(annotation.username(), "password", null);
+                new UsernamePasswordAuthenticationToken(userJoinRequest.getUserName(), userJoinRequest.getPassword(), null);
+
+        final SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+
         securityContext.setAuthentication(authenticationToken);
+
         return securityContext;
     }
 }
